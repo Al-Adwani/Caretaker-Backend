7250cbc61c8f413520c8f352ec9d35ce94490c2f
hassan alhassaini <hassandak1d@hotmail.com>
2021-12-06 16:35:22 +0300

    sligh changes

--- api/appointment/appointment.controllers.js
+++ api/appointment/appointment.controllers.js
@@ -1,4 +1,16 @@
+const { findById } = require("../../models/Appointments");
+const Appointments = require("../../models/Appointments");
 const Appointment = require("../../models/Appointments");
+
+// get appointment by Id
+exports.fetchAppointment = async (appointmentId, next) => {
+  try {
+    const appointment = await Appointment.findById(appointmentId);
+    return appointment;
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.bookAppointment = async (req, res, next) => {
   try {
@@ -23,3 +35,27 @@ exports.AppointListFetch = async (req, res, next) => {
     next(error);
   }
 };
+
+// delete a appointment
+exports.appointmentDelete = async (req, res, next) => {
+  try {
+    await req.appointment.remove();
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+};
+
+//updating appointment
+// exports.updateAppointment = async (req, res, next) => {
+//   // try {
+//     console.log(appointmentId.status);
+//     const status = await Appointment.findById(appointmentId.status);
+//     const appointment = await Appointment.findByIdAndUpdate(appointmentId, {
+//       $set: { status: !status },
+//     });
+//     return res.status(200).json(appointment);
+//   // } catch (error) {
+//   //   next(error);
+//   // }
+// };
