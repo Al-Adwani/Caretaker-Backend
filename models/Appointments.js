8fb421b3133e089e3376b5f29e79190aacecd2e4
Abdullah Al Adwani <abdullahmmaladwani@gmail.com>
2021-12-06 19:02:27 +0300

    Booking Appointment

--- models/Appointments.js
+++ models/Appointments.js
@@ -3,7 +3,9 @@ const { model, Schema } = require("mongoose");
 const AppointmentSchema = Schema(
   {
     guardian: { type: Schema.Types.ObjectId, ref: "Guardian" },
+    guardianName:{type: String},
     caretaker: { type: Schema.Types.ObjectId, ref: "Caretaker" },
+    caretakerName:{type: String},
     status: { type: String, required: true },
   },
   { timestamps: true }
